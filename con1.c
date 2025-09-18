033ca4860e4c49b9334c97280423db7634067843
vishvaefx <mmurugantamilselvi@gmail.com>
2025-09-18 13:49:10 +0530

    Add files via upload

---
+++ con1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+ int a,b;
+ printf ("enter two numbers:");
+ scanf("%d",&a);
+ scanf("%d",&b);
+ if (a>b)
+ printf("a is greater");
+ else{
+ 	printf("else b is greater");
+ 	
+ }
+ return 0;
+ 
+}
