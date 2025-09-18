5f63d79debee835c87529035df28233ab96ab903
vishvaefx <mmurugantamilselvi@gmail.com>
2025-09-18 13:26:56 +0530

    Add files via upload

---
+++ con12.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int a;
+ printf ("enter two numbers");
+ scanf("%d",&a);
+ if (a%2==0)
+ printf("given value is divisible by 2");
+ else{
+ 	printf("given value is not divisible by 2");
+ 	
+ }
+ return 0;
+ 
+}
