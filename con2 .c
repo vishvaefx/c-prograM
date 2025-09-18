d1ac9c3e9b893e9304780479ca688a95afd44e70
vishvaefx <mmurugantamilselvi@gmail.com>
2025-09-18 13:51:32 +0530

    Add files via upload

---
+++ con2 .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+ int a,b;
+ printf ("enter two numbers:");
+ scanf("%d",&a);
+ scanf("%d",&b);
+ if (a>b)
+ printf("b is smaller");
+ else{
+ 	printf("a is smaller");
+ 	
+ }
+ return 0;
+ 
+}
