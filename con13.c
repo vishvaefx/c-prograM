bb4178c1cf126d754991e7edcee08967dc79e108
vishvaefx <mmurugantamilselvi@gmail.com>
2025-09-18 13:32:59 +0530

    Add files via upload

---
+++ con13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int a;
+ printf ("enter a number");
+ scanf("%d",&a);
+ if (a>0)
+ printf("given value is +ve");
+ if (a<0)
+ printf("given value is -ve");
+ if (a==0)
+ printf("given value is 0"); 
+ return 0;
+ 
+}
